cf665090f1914ce9b36b75a93341716f72e48059
K. Shankari <shankari@eecs.berkeley.edu>
2016-01-05 11:54:18 -0800

    Check in sample native settings screens and hooks to copy them to the platform directories

---
+++ emTracker/extra_native_src/ios/NativeSettings.h
@@ -0,0 +1,13 @@
+//
+//  NativeSettings.h
+//  referenceSidebarApp
+//
+//  Created by Kalyanaraman Shankari on 1/5/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NativeSettings : UIViewController
+
+@end
